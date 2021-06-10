f2ffe58fbfda6ff23218428313308f83ba8d48f4
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-06-10 22:05:13 +1000

    feat: add LogCorrelationMiddleware and RequestCorrelationService

---
+++ Tsukiy0.Extensions.Logging.AspNetCore/Middlewares/LogCorrelationMiddleware.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Tsukiy0.Extensions.Logging.Core.Extensions;
+using Tsukiy0.Extensions.Logging.Core.Services;
+
+namespace Tsukiy0.Extensions.Logging.AspNetCore.Middlewares
+{
+    public class LogCorrelationMiddleware : IMiddleware
+    {
+        private readonly ICorrelationService correlationService;
+
+        public LogCorrelationMiddleware(ICorrelationService correlationService)
+        {
+            this.correlationService = correlationService;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var logger = context.RequestServices.GetRequiredService<ILogger<LogCorrelationMiddleware>>();
+            await logger.WithCorrelation(correlationService, async () =>
+            {
+                await next(context);
+            });
+        }
+    }
+
+}
