c72dac4cec180fb28104469efb69f019022119ce
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-06-11 23:00:02 +1000

    feat: add separate single and batch SqsRuntime

--- Tsukiy0.Extensions.Aws.Runtime.Lambda/BatchSqsRuntime.cs
+++ Tsukiy0.Extensions.Aws.Runtime.Lambda/BatchSqsRuntime.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Amazon.Lambda.SQSEvents;
@@ -8,21 +9,21 @@ using Microsoft.Extensions.Logging;
 using NLog;
 using NLog.Extensions.Logging;
 using Tsukiy0.Extensions.Aws.Core.Models;
+using Tsukiy0.Extensions.Aws.Core.Services;
+using Tsukiy0.Extensions.Aws.Infrastructure.Services;
 using Tsukiy0.Extensions.Logging.Core.Extensions;
 using Tsukiy0.Extensions.Logging.Core.Services;
 using Tsukiy0.Extensions.Logging.NLog.Extensions;
 
 namespace Tsukiy0.Extensions.Aws.Runtime.Lambda
 {
-    public abstract class SqsRuntime<T>
+    public abstract class BatchSqsRuntime<T>
     {
         protected abstract void ConfigureServices(HostBuilderContext ctx, IServiceCollection services);
         protected abstract Task Run(IHost host, T message);
 
         private IHost CreateHost(SendMessageEnvelope<T> message)
         {
-            LogManager.Setup().LoadConfiguration(_ => _.Configuration.AddStandardLog());
-
             return Host.CreateDefaultBuilder()
                 .ConfigureServices((ctx, services) =>
                 {
@@ -34,6 +35,7 @@ namespace Tsukiy0.Extensions.Aws.Runtime.Lambda
                     });
                     services.AddScoped(_ => message);
                     services.AddScoped<ICorrelationService, SqsCorrelationService<T>>();
+                    services.AddScoped<ISqsClient<T>, SqsClient<T>>();
 
                     ConfigureServices(ctx, services);
                 })
@@ -42,23 +44,31 @@ namespace Tsukiy0.Extensions.Aws.Runtime.Lambda
 
         public async Task FunctionHandler(SQSEvent input)
         {
-            var message = JsonSerializer.Deserialize<SendMessageEnvelope<T>>(input.Records[0].Body);
-            var host = CreateHost(message);
-            var logger = host.Services.GetRequiredService<ILogger<SqsRuntime<T>>>();
-            var correlationService = host.Services.GetRequiredService<ICorrelationService>();
-
-            await logger.WithCorrelation(correlationService, async () =>
+            var tasks = input.Records.Select(async _ =>
             {
-                try
+                LogManager.Setup().LoadConfiguration(_ => _.Configuration.AddStandardLog());
+                var message = JsonSerializer.Deserialize<SendMessageEnvelope<T>>(_.Body);
+                var host = CreateHost(message);
+                var logger = host.Services.GetRequiredService<ILogger<SqsRuntime<T>>>();
+                var sqsClient = host.Services.GetRequiredService<ISqsClient<T>>();
+                var correlationService = host.Services.GetRequiredService<ICorrelationService>();
+
+                await logger.WithCorrelation(correlationService, async () =>
                 {
-                    logger.LogInformation("New message: {message}", message);
-                    await Run(host, message.Message);
-                }
-                catch (Exception e)
-                {
-                    logger.LogError(e, "Failed to process message");
-                }
+                    try
+                    {
+                        logger.LogInformation("New message: {message}", _);
+                        await Run(host, message.Message);
+                        await sqsClient.Delete(_.ReceiptHandle);
+                    }
+                    catch (Exception e)
+                    {
+                        logger.LogError(e, "Failed to process message");
+                    }
+                });
             });
+
+            await Task.WhenAll(tasks);
         }
     }
 }
--- Tsukiy0.Extensions.Aws.Runtime.Lambda/services/SqsCorrelationService.cs
+++ Tsukiy0.Extensions.Aws.Runtime.Lambda/services/SqsCorrelationService.cs
@@ -1,34 +1,35 @@
 using System;
-using System.Text.Json;
 using Tsukiy0.Extensions.Aws.Core.Models;
 using Tsukiy0.Extensions.Logging.Core.Services;
-using static Amazon.Lambda.SQSEvents.SQSEvent;
 
 namespace Tsukiy0.Extensions.Aws.Runtime.Lambda
 {
-    public class SqsCorrelationService : ICorrelationService
+    public class SqsCorrelationService<T> : ICorrelationService
     {
         public Guid TraceId { get; }
 
         public Guid SpanId { get; }
 
-        public SqsCorrelationService(SQSMessage message)
+        public SqsCorrelationService(SendMessageEnvelope<T> message)
         {
             TraceId = GetTraceId(message);
             SpanId = Guid.NewGuid();
         }
 
-        private Guid GetTraceId(SQSMessage message)
+        private Guid GetTraceId(SendMessageEnvelope<T> message)
         {
-            var raw = JsonSerializer.Deserialize<SendMessageEnvelope<object>>(message.Body);
+            try
+            {
+                if (message.TraceId == Guid.Empty)
+                {
+                    return Guid.NewGuid();
+                }
 
-            if (raw is null || raw.TraceId == Guid.Empty)
+                return message.TraceId;
+            }
+            catch
             {
                 return Guid.NewGuid();
-            }
-            else
-            {
-                return raw.TraceId;
             }
         }
     }
