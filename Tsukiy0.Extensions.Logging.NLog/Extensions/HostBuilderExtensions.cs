811fd6193ab2d0963281e4775c3a601516a26f8c
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-06-15 19:28:08 +1000

    feat: add HostBuilderExtensions

---
+++ Tsukiy0.Extensions.Logging.NLog/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,42 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Config;
+using NLog.Extensions.Hosting;
+using NLog.LayoutRenderers;
+using NLog.Layouts;
+using NLog.Targets;
+using Tsukiy0.Extensions.NLog.Renderers;
+
+namespace Tsukiy0.Extensions.Logging.Nlog.Extensions
+{
+    public static class HostBuilderExtensions
+    {
+        public static IHostBuilder AddSharedLogging(this IHostBuilder builder)
+        {
+            LogManager.Configuration = BuildConfig();
+            return builder
+                .ConfigureLogging((_, logging) =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog();
+        }
+
+        private static LoggingConfiguration BuildConfig()
+        {
+            var config = new LoggingConfiguration();
+            LayoutRenderer.Register<LogLayoutRenderer>("shared-log");
+
+            var consoleTarget = new ConsoleTarget()
+            {
+                Layout = Layout.FromString("${shared-log}")
+            };
+
+            config.AddRuleForAllLevels(consoleTarget);
+
+            return config;
+        }
+    }
+}
