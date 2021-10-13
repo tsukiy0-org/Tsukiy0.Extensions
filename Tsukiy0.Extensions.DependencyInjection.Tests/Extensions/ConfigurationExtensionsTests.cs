7785e1c1a68e76efbe47742b07195d2dddf750e4
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-10-13 20:45:42 +1100

    feat: add ConfigurationExtensions

---
+++ Tsukiy0.Extensions.DependencyInjection.Tests/Extensions/ConfigurationExtensionsTests.cs
@@ -0,0 +1,62 @@
+namespace Tsukiy0.Extensions.DependencyInjection.Tests.Extensions
+{
+    public class ConfigurationExtensionsTests
+    {
+        [Fact]
+        public void GetSection_ByType()
+        {
+            //Arange
+            var configuration = SetupConfiguration(new Dictionary<string, string>
+            {
+                {"TestConfig:A", "1"},
+                {"TestConfig:B", "2"}
+            });
+
+            //Act
+            var config = configuration.GetSection<TestConfig>();
+
+            //Assert
+            config.Should().BeEquivalentTo(new TestConfig
+            {
+                A = "1",
+                B = "2"
+            });
+        }
+
+        [Fact]
+        public void GetSection_ByName()
+        {
+            //Arange
+            var configuration = SetupConfiguration(new Dictionary<string, string>
+            {
+                {"SomethingRandom:A", "1"},
+                {"SomethingRandom:B", "2"}
+            });
+
+            //Act
+            var config = configuration.GetSection<TestConfig>("SomethingRandom");
+
+            //Assert
+            config.Should().BeEquivalentTo(new TestConfig
+            {
+                A = "1",
+                B = "2"
+            });
+        }
+
+        private class TestConfig
+        {
+            public string A { get; set; }
+            public string B { get; set; }
+            public string C { get; set; }
+            public string D { get; set; }
+        }
+
+        private IConfiguration SetupConfiguration(IDictionary<string, string> config)
+        {
+            return new ConfigurationBuilder()
+                .AddInMemoryCollection(config)
+                .Build();
+        }
+    }
+}
---
+++ Tsukiy0.Extensions.DependencyInjection/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Tsukiy0.Extensions.DependencyInjection.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static T GetSection<T>(this IConfiguration configuration) where T : class
+        {
+            var classType = typeof(T);
+            var className = classType.Name;
+            return configuration.GetSection(className).Get<T>();
+        }
+
+        public static T GetSection<T>(this IConfiguration configuration, string key)
+        {
+            return configuration.GetSection(key).Get<T>();
+        }
+    }
+}
