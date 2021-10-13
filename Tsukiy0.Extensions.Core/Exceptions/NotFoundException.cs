83d8c3495099e872dbe84b0a71992c43ea5b443b
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-10-13 20:14:39 +1100

    feat: add NotFoundException

---
+++ Tsukiy0.Extensions.Core/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Tsukiy0.Extensions.Core.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string? message) : base(message) { }
+    }
+}
