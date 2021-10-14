7cc38286a3db2c1c07bfc3ad79273f529f4b78fc
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-10-14 22:57:05 +1100

    feat: add LogCorrelationMiddleware and LogRequestMiddleware to aspnet

---
+++ Tsukiy0.Extensions.AspNetCore/Middlewares/LogCorrelationMiddleware.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Tsukiy0.Extensions.Correlation.Services;
+using Tsukiy0.Extensions.Logging.Extensions;
+
+namespace Tsukiy0.Extensions.AspNetCore.Middlewares
+{
+    public class LogCorrelationMiddleware : IMiddleware
+    {
+        private readonly ICorrelationService correlationService;
+
+        public LogCorrelationMiddleware(ICorrelationService correlationService)
+        {
+            this.correlationService = correlationService;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var logger = context.RequestServices.GetRequiredService<ILogger<LogCorrelationMiddleware>>();
+            await logger.WithCorrelation(correlationService, async () =>
+            {
+                await next(context);
+            });
+        }
+    }
+
+}
---
+++ Tsukiy0.Extensions.AspNetCore/Middlewares/LogRequestMiddleware.cs
@@ -0,0 +1,47 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Tsukiy0.Extensions.Logging.AspNetCore.Middlewares
+{
+    public class LogRequestMiddleware : IMiddleware
+    {
+        private readonly ILogger<LogRequestMiddleware> logger;
+
+        public LogRequestMiddleware(ILogger<LogRequestMiddleware> logger)
+        {
+            this.logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            finally
+            {
+                logger.LogInformation(
+                    "Transaction {Request} {Response}",
+                    new
+                    {
+                        Host = context.Request.Host.Value,
+                        Scheme = context.Request.Scheme,
+                        Protocol = context.Request.Protocol,
+                        Method = context.Request.Method,
+                        Path = context.Request.Path.Value,
+                        Query = context.Request.QueryString.Value,
+                        ContentType = context.Request.ContentType,
+                        ContentLength = context.Request.ContentLength
+                    },
+                    new
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        ContentType = context.Response.ContentType,
+                        ContentLength = context.Response.ContentLength
+                    }
+                );
+            }
+        }
+    }
+}
