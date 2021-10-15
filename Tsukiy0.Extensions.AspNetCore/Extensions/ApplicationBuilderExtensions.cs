2f2252a85820ac184bf967edb1a5c3b8843e5b49
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-10-15 22:16:38 +1100

    feat: add ApplicationBuilderExtensions

---
+++ Tsukiy0.Extensions.AspNetCore/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Tsukiy0.Extensions.AspNetCore.Middlewares;
+using Tsukiy0.Extensions.DependencyInjection.Extensions;
+using Tsukiy0.Extensions.Logging.AspNetCore.Middlewares;
+
+namespace Tsukiy0.Extensions.AspNetCore.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseDefaultLogging(this IApplicationBuilder app)
+        {
+            return app
+                .UseMiddleware<LogCorrelationMiddleware>()
+                .UseMiddleware<LogRequestMiddleware>();
+        }
+
+        public static IApplicationBuilder UseCorsWhitelist(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            var origins = configuration.GetSection<string[]>("CorsWhitelist");
+
+            if (origins is null)
+            {
+                return app;
+            }
+
+            return app.UseCors(_ =>
+            {
+                _.SetIsOriginAllowedToAllowWildcardSubdomains()
+                    .WithOrigins(origins)
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+        }
+
+        public static void UseErrorHandling(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+        }
+    }
+}
