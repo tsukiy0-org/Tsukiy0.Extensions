1872149c1e969d1698fe86cba45ed8f9e6a3ab8d
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-10-16 23:23:46 +1100

    feat: impl DynamoVersionScanMigrator

--- Tsukiy0.Extensions.Data.Aws/Services/DynamoVersionScanMigrator.cs
+++ Tsukiy0.Extensions.Data.Aws/Services/DynamoVersionScanMigrator.cs
@@ -1,43 +1,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 using Tsukiy0.Extensions.Data.Aws.Configs;
+using Tsukiy0.Extensions.Data.Aws.Extensions;
 using Tsukiy0.Extensions.Data.Services;
 
 namespace Tsukiy0.Extensions.Data.Aws.Services
 {
     public class DynamoVersionScanMigrator<T>
     {
-        // private readonly AbstractVersionDaoMapper<T, Dictionary<string, AttributeValue>> mapper;
-        // private readonly IAmazonDynamoDB client;
-        // private readonly DynamoVersionScanMigratorConfig config;
+        private readonly AbstractVersionDaoMapper<T, Dictionary<string, AttributeValue>> mapper;
+        private readonly IAmazonDynamoDB client;
+        private readonly DynamoVersionScanMigratorConfig config;
 
-        // public DynamoScanMigrator(BaseDynamoMapper<T> mapper, IAmazonDynamoDB client, DynamoScanMigratorConfig config)
-        // {
-        //     this.mapper = mapper;
-        //     this.client = client;
-        //     this.config = config;
-        // }
+        public DynamoVersionScanMigrator(AbstractVersionDaoMapper<T, Dictionary<string, AttributeValue>> mapper, IAmazonDynamoDB client, DynamoVersionScanMigratorConfig config)
+        {
+            this.mapper = mapper;
+            this.client = client;
+            this.config = config;
+        }
 
-        // public async Task Migrate()
-        // {
-        //     await client.ForEachScannedPage(
-        //         new ScanRequest
-        //         {
-        //             TableName = config.TableName,
-        //             Limit = config.Limit,
-        //         },
-        //         async (IEnumerable<Dictionary<string, AttributeValue>> items) =>
-        //         {
-        //             var daos = await Task.WhenAll(items.Select(async _ =>
-        //             {
-        //                 var dto = await mapper.ToDto(_);
-        //                 return await mapper.ToDao(dto);
-        //             }));
-
-        //             await client.BatchPut(config.TableName, daos);
-        //         }
-        //     );
-        // }
+        public async Task Migrate()
+        {
+            await client.ScanAllAsync(
+                new ScanRequest
+                {
+                    TableName = config.TableName,
+                }
+            )
+                .Buffer(25)
+                .SelectAwait(async items =>
+                {
+                    return await Task.WhenAll(items.Select(async _ =>
+                    {
+                        var dest = await mapper.From(_);
+                        return await mapper.To(dest);
+                    }));
+                })
+                .ForEachAsync(async _ =>
+                {
+                    await client.PutAll(config.TableName, _);
+                });
+        }
     }
 }
