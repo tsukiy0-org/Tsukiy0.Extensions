734a1b65778fb28ff58a89c5e243c3e654ea1f2d
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-10-16 11:38:30 +1100

    feat: remove aws proj

--- Tsukiy0.Extensions.Aws.Core/Models/ReceiveMessageEnvelope.cs
+++
@@ -1,6 +0,0 @@
-using System;
-
-namespace Tsukiy0.Extensions.Aws.Core.Models
-{
-    public record ReceiveMessageEnvelope<T>(string ReceiptHandle, Guid TraceId, T Message);
-}
--- Tsukiy0.Extensions.Aws.Core/Models/SendMessageEnvelope.cs
+++
@@ -1,6 +0,0 @@
-using System;
-
-namespace Tsukiy0.Extensions.Aws.Core.Models
-{
-    public record SendMessageEnvelope<T>(Guid TraceId, T Message);
-}
