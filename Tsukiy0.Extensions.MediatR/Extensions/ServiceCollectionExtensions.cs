0d6fa6c50a887c6bd04400b1f1123eec9d7408db
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-10-27 19:08:08 +1100

    feat: add AddMediatR helper

---
+++ Tsukiy0.Extensions.MediatR/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tsukiy0.Extensions.MediatR.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMediatR(this IServiceCollection _)
+        {
+            _.AddScoped<ServiceFactory>(_ => type => _.GetService(type));
+            _.AddScoped<IMediator, Mediator>();
+            return _;
+        }
+    }
+}
