5d70cea11d4fc81f83b8fcfdc20b3bca8b1bad32
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-10-27 19:43:53 +1100

    feat: add AddSecretsManagerConfiguration extension

--- Tsukiy0.Extensions.Configuration.Aws/Extensions/ConfigurationBuilderExtensions.cs
+++ Tsukiy0.Extensions.Configuration.Aws/Extensions/ConfigurationBuilderExtensions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Amazon.SecretsManager;
 using Amazon.SimpleSystemsManagement;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
@@ -12,6 +13,11 @@ namespace Tsukiy0.Extensions.Configuration.Aws.Extensions
         public static IConfigurationBuilder AddSsmParameterConfiguration(this IConfigurationBuilder builder, IList<SsmParameterMap> maps)
         {
             return builder.Add(new SsmParameterConfigurationSource(maps));
+        }
+
+        public static IConfigurationBuilder AddSecretsManagerConfiguration(this IConfigurationBuilder builder, IList<SecretsManagerMap> maps)
+        {
+            return builder.Add(new SecretsManagerConfigurationSource(maps));
         }
 
         private class SsmParameterConfigurationSource : IConfigurationSource
@@ -28,6 +34,23 @@ namespace Tsukiy0.Extensions.Configuration.Aws.Extensions
             {
                 var service = new SsmParameterService(new AmazonSimpleSystemsManagementClient(), CACHE);
                 return new SsmParameterConfigurationProvider(service, _maps);
+            }
+        }
+
+        private class SecretsManagerConfigurationSource : IConfigurationSource
+        {
+            private readonly static IMemoryCache CACHE = new MemoryCache(new MemoryCacheOptions());
+            private readonly IList<SecretsManagerMap> _maps;
+
+            public SecretsManagerConfigurationSource(IList<SecretsManagerMap> maps)
+            {
+                _maps = maps;
+            }
+
+            public IConfigurationProvider Build(IConfigurationBuilder builder)
+            {
+                var service = new SecretsManagerService(new AmazonSecretsManagerClient(), CACHE);
+                return new SecretsManagerConfigurationProvider(service, _maps);
             }
         }
     }
