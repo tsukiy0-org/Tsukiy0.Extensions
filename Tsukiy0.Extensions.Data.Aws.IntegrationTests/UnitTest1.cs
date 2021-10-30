bb17d0c4facc66226e22823043dabeaf2ad873cf
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-10-30 19:23:48 +1100

    chore: stub out data.aws integration tests

---
+++ Tsukiy0.Extensions.Data.Aws.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Tsukiy0.Extensions.Data.Aws.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
