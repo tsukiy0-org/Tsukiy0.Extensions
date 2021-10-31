0e4421bba21c38741ab9e0b38f552fbff217fd61
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-10-31 15:35:53 +1100

    chore: update AwsProcessor to save TestModel

--- Tsukiy0.Extensions.Example.Processor.Aws/Sqs/Function.cs
+++ Tsukiy0.Extensions.Example.Processor.Aws/Sqs/Function.cs
@@ -1,12 +1,12 @@
-using System;
 using Amazon.Lambda.Core;
+using Tsukiy0.Extensions.Example.Core.Models;
 using Tsukiy0.Extensions.Processor.Aws.Runtimes;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace Tsukiy0.Extensions.Example.Processor.Aws.Sqs
 {
-    public class Function : SqsLambdaProcessorRuntime<Guid>
+    public class Function : SqsLambdaProcessorRuntime<TestModel>
     {
         public Function() : base(new Processor()) { }
     }
--- Tsukiy0.Extensions.Example.Processor.Aws/Sqs/Processor.cs
+++ Tsukiy0.Extensions.Example.Processor.Aws/Sqs/Processor.cs
@@ -1,25 +1,47 @@
-using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Tsukiy0.Extensions.Configuration.Aws.Extensions;
+using Tsukiy0.Extensions.Configuration.Aws.Models;
+using Tsukiy0.Extensions.Configuration.Extensions;
+using Tsukiy0.Extensions.Data.Aws.Services;
+using Tsukiy0.Extensions.Example.Core.Models;
+using Tsukiy0.Extensions.Example.Infrastructure.Configs;
+using Tsukiy0.Extensions.Example.Infrastructure.Services;
 using Tsukiy0.Extensions.Logging.NLog.Extensions;
 using Tsukiy0.Extensions.Processor.Services;
 
 namespace Tsukiy0.Extensions.Example.Processor.Aws.Sqs
 {
-    public class Processor : DefaultProcessor<Guid>
+    public class Processor : DefaultProcessor<TestModel>
     {
         protected override IHostBuilder ConfigureHost(IHostBuilder builder)
         {
             return builder
-                .ConfigureNLogLogging(nameof(Processor))
+                .ConfigureNLogLogging(typeof(Processor).Namespace)
+                .ConfigureAppConfiguration(_ =>
+                {
+                    _.AddSsmParameterConfiguration(new List<SsmParameterMap>
+                    {
+                        new SsmParameterMap("/tsukiy0/extensions/test-dynamo-table/table-name", $"{nameof(TestModelDynamoRepositoryConfig)}:{nameof(TestModelDynamoRepositoryConfig.TableName)}")
+                    });
+                })
                 .ConfigureServices((ctx, _) =>
                 {
+                    _.AddConfig<TestModelDynamoRepositoryConfig>(ctx.Configuration);
+                    _.AddAWSService<IAmazonDynamoDB>();
+                    _.AddScoped<TestModelV1DaoMapper>();
+                    _.AddScoped<IDynamoDaoMapper<TestModel>, TestModelVersionDaoMapper>();
+                    _.AddScoped<TestModelDynamoRepository>();
                 });
         }
 
-        protected override Task Run(IHost host, Guid body)
+        protected override async Task Run(IHost host, TestModel body)
         {
-            throw new NotImplementedException();
+            var repo = host.Services.GetRequiredService<TestModelDynamoRepository>();
+            await repo.PutAll(new List<TestModel> { body });
         }
     }
 
