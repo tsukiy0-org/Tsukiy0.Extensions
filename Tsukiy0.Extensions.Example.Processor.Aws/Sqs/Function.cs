9c94622e310ff1a594157be6f18c24e1395c820b
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-10-31 23:52:36 +1100

    chore: use SaveTestModelHandler in sqs processor

--- Tsukiy0.Extensions.Example.Processor.Aws/Sqs/Function.cs
+++ Tsukiy0.Extensions.Example.Processor.Aws/Sqs/Function.cs
@@ -1,12 +1,12 @@
 using Amazon.Lambda.Core;
-using Tsukiy0.Extensions.Example.Core.Models;
+using Tsukiy0.Extensions.Example.Core.Handlers;
 using Tsukiy0.Extensions.Processor.Aws.Runtimes;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace Tsukiy0.Extensions.Example.Processor.Aws.Sqs
 {
-    public class Function : SqsLambdaProcessorRuntime<TestModel>
+    public class Function : SqsLambdaProcessorRuntime<SaveTestModelRequest>
     {
         public Function() : base(new Processor()) { }
     }
--- Tsukiy0.Extensions.Example.Processor.Aws/Sqs/Processor.cs
+++ Tsukiy0.Extensions.Example.Processor.Aws/Sqs/Processor.cs
@@ -1,21 +1,24 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Amazon.DynamoDBv2;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Tsukiy0.Extensions.Configuration.Aws.Extensions;
 using Tsukiy0.Extensions.Configuration.Aws.Models;
 using Tsukiy0.Extensions.Configuration.Extensions;
 using Tsukiy0.Extensions.Data.Aws.Services;
+using Tsukiy0.Extensions.Example.Core.Handlers;
 using Tsukiy0.Extensions.Example.Core.Models;
 using Tsukiy0.Extensions.Example.Infrastructure.Configs;
 using Tsukiy0.Extensions.Example.Infrastructure.Services;
 using Tsukiy0.Extensions.Logging.NLog.Extensions;
+using Tsukiy0.Extensions.MediatR.Extensions;
 using Tsukiy0.Extensions.Processor.Services;
 
 namespace Tsukiy0.Extensions.Example.Processor.Aws.Sqs
 {
-    public class Processor : DefaultProcessor<TestModel>
+    public class Processor : DefaultProcessor<SaveTestModelRequest>
     {
         protected override IHostBuilder ConfigureHost(IHostBuilder builder)
         {
@@ -35,13 +38,16 @@ namespace Tsukiy0.Extensions.Example.Processor.Aws.Sqs
                     _.AddScoped<TestModelV1DaoMapper>();
                     _.AddScoped<IDynamoDaoMapper<TestModel>, TestModelVersionDaoMapper>();
                     _.AddScoped<DynamoTestModelRepository>();
+
+                    _.AddMediatR();
+                    _.AddScoped<IRequestHandler<SaveTestModelRequest, Unit>, SaveTestModelHandler>();
                 });
         }
 
-        protected override async Task Run(IHost host, TestModel body)
+        protected override async Task Run(IHost host, SaveTestModelRequest body)
         {
-            var repo = host.Services.GetRequiredService<DynamoTestModelRepository>();
-            await repo.PutAll(new List<TestModel> { body });
+            var mediator = host.Services.GetRequiredService<Mediator>();
+            await mediator.Send(body);
         }
     }
 
