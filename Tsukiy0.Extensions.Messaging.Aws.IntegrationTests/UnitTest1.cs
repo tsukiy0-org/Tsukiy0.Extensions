b9008c1a27ea7719d3743b1ca6cb0f30c9145707
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-10-31 23:59:10 +1100

    chore: stub out message.aws.integrationtests

---
+++ Tsukiy0.Extensions.Messaging.Aws.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Tsukiy0.Extensions.Messaging.Aws.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
