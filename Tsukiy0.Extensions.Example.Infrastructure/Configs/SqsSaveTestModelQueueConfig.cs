a5eb57de48b740e299fccaa7c2a1947ef331e6b2
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-10-31 23:57:35 +1100

    chore: add SqsSaveTestModelQueue

---
+++ Tsukiy0.Extensions.Example.Infrastructure/Configs/SqsSaveTestModelQueueConfig.cs
@@ -0,0 +1,7 @@
+namespace Tsukiy0.Extensions.Example.Infrastructure.Configs
+{
+    public record SqsSaveTestModelQueueConfig
+    {
+        public string QueueUrl { get; set; }
+    }
+}
---
+++ Tsukiy0.Extensions.Example.Infrastructure/Services/SqsSaveTestModelQueue.cs
@@ -0,0 +1,19 @@
+
+using Amazon.SQS;
+using Tsukiy0.Extensions.Correlation.Services;
+using Tsukiy0.Extensions.Example.Core.Handlers;
+using Tsukiy0.Extensions.Example.Infrastructure.Configs;
+using Tsukiy0.Extensions.Messaging.Aws.Services;
+using Tsukiy0.Extensions.Messaging.Services;
+
+namespace Tsukiy0.Extensions.Example.Infrastructure.Services
+{
+    public class SqsSaveTestModelQueue : DefaultQueue<SaveTestModelRequest>
+    {
+        public SqsSaveTestModelQueue(IAmazonSQS sqs, SqsSaveTestModelQueueConfig config, ICorrelationService correlationService)
+        : base(new SqsMessageQueue<SaveTestModelRequest>(sqs, config.QueueUrl), correlationService)
+        {
+        }
+    }
+
+}
