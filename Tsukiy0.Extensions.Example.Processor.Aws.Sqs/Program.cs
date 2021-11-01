4ba35fd772db98687f54eaab3b7217569ef14a2d
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-11-01 20:56:20 +1100

    chore: impl main

---
+++ Tsukiy0.Extensions.Example.Processor.Aws.Sqs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Tsukiy0.Extensions.Example.Processor.Aws.Sqs
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
