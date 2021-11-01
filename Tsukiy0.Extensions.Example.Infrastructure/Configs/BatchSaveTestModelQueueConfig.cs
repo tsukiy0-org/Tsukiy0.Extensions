beb746a322bf982e7446019ba18b25f64eab91c8
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-11-01 20:41:26 +1100

    chore: remove unimplemented method on config

--- Tsukiy0.Extensions.Example.Infrastructure/Configs/BatchSaveTestModelQueueConfig.cs
+++ Tsukiy0.Extensions.Example.Infrastructure/Configs/BatchSaveTestModelQueueConfig.cs
@@ -1,15 +1,8 @@
-using System;
-
 namespace Tsukiy0.Extensions.Example.Infrastructure.Configs
 {
     public record BatchSaveTestModelQueueConfig
     {
         public string JobDefinitionArn { get; set; }
         public string JobQueueArn { get; set; }
-
-        public static object JobQueueUrl()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
