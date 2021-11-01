3a4dd0ce00e86750ae1c11d50230acf1d4046c73
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-11-01 19:15:46 +1100

    chore: add batch processor

---
+++ Tsukiy0.Extensions.Example.Processor.Aws.Batch/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Tsukiy0.Extensions.Example.Core.Handlers;
+using Tsukiy0.Extensions.Example.Infrastructure.Services;
+using Tsukiy0.Extensions.Processor.Aws.Runtimes;
+
+namespace Tsukiy0.Extensions.Example.Processor.Aws.Batch
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var runtime = new BatchProcessorRuntime<SaveTestModelRequest>(new SaveTestModelProcessor());
+            await runtime.Run();
+        }
+    }
+}
