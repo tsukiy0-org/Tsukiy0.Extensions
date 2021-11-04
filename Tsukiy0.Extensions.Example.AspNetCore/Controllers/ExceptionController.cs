509a255ae184c059d8574cfd579f99acf439c754
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-11-04 19:02:04 +1100

    chore: add exception controller

---
+++ Tsukiy0.Extensions.Example.AspNetCore/Controllers/ExceptionController.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Tsukiy0.Extensions.AspNetCore.Filters;
+using Tsukiy0.Extensions.Core.Exceptions;
+
+namespace Tsukiy0.Extensions.Example.AspNetCore.Controllers
+{
+    [ApiController]
+    [Route("v1/Exceptions")]
+    [ApiExplorerSettings(GroupName = "v1")]
+    [ApiKeyAuth]
+    public class ExceptionController : ControllerBase
+    {
+        [HttpGet("Validation")]
+        public void Validation()
+        {
+            throw new ValidationException("");
+        }
+
+        [HttpGet("NotFound")]
+        public void NotFound()
+        {
+            throw new NotFoundException("");
+        }
+
+        [HttpGet("Unknown")]
+        public void Unknown()
+        {
+            throw new SystemException("");
+        }
+    }
+}
+
+
