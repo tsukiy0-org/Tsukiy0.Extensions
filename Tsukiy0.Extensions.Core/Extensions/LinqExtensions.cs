b5f2a263109b0a9896c94908924db37b4bf3cc5a
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-11-10 20:45:53 +1100

    feat: bump to net6.0
    * remove Chunk

--- Tsukiy0.Extensions.Core/Extensions/LinqExtensions.cs
+++ Tsukiy0.Extensions.Core/Extensions/LinqExtensions.cs
@@ -5,11 +5,5 @@ namespace Tsukiy0.Extensions.Core.Extensions
 {
     public static class LinqExtensions
     {
-        public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> list, int size)
-        {
-            return list.Select((_, i) => new { Value = _, Index = i })
-                .GroupBy(_ => _.Index / size)
-                .Select(_ => _.Select(_ => _.Value));
-        }
     }
 }
